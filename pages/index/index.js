cd531e258d6794f20188e0b838f130fe6408776e
nick.zhuyj <nick.zhuyj@cn.ibm.com>
2017-11-23 11:09:22 +0800

    call rest with common.get

--- pages/index/index.js
+++ pages/index/index.js
@@ -54,51 +54,44 @@ Page({
           var city = res.data.result.addressComponent.city;
           weather.city = city;
           console.log('city=' + city);
-          wx.request({
-            url: common.endpoint + '/weather_mini?city=' + city,
-            header: {
-              'content-type': 'application/json'
-            },
-            success: function (res) {
-              var data = res.data;
-              if (data.status === 1000) {
-                var today = {};
-                today.wendu = data.data.wendu;          //温度
-                var forecast = data.data.forecast;      //天气特征与未来天气
-                var todayWeather = forecast[0];         //今天的天气
-                today.low = todayWeather.low.split(' ')[1];    //最低温
-                today.high = todayWeather.high.split(' ')[1];  //最高温
-                var typeText = todayWeather.type;
-                today.typeText = typeText;                     //天气类型说明
-                today.week = todayWeather.date.slice(3);       //星期几
-                today.typeIcon = typeIcon[typeText];           //天气类型图片
-                today.typeBackgorund = "background-default";
-                weather.today = today;
+          common.get('/weather_mini?city=' + city).then(res => {
+            var data = res.data;
+            if (data.status === 1000) {
+              var today = {};
+              today.wendu = data.data.wendu;          //温度
+              var forecast = data.data.forecast;      //天气特征与未来天气
+              var todayWeather = forecast[0];         //今天的天气
+              today.low = todayWeather.low.split(' ')[1];    //最低温
+              today.high = todayWeather.high.split(' ')[1];  //最高温
+              var typeText = todayWeather.type;
+              today.typeText = typeText;                     //天气类型说明
+              today.week = todayWeather.date.slice(3);       //星期几
+              today.typeIcon = typeIcon[typeText];           //天气类型图片
+              today.typeBackgorund = "background-default";
+              weather.today = today;
 
-                //下周天气、未来天气
-                var temp, futureList = [];
-                for (var i = 1; i < forecast.length; i++) {
-                  temp = forecast[i];
-                  var future = {};
-                  future.week = temp.date.slice(3);
-                  future.type = typeIcon[temp.type];
-                  var wendurange = temp.low.split(' ')[1] + "-" + temp.high.split(' ')[1];
-                  future.wendu = wendurange;
-                  future.typeTetx = temp.type;
-                  futureList.push(future);
-                }
-
-                weather.futureList = futureList;
-                console.log(weather);
-                that.setData({
-                  weather: weather,
-                  Loadinghidden: true
-                });
+              //下周天气、未来天气
+              var temp, futureList = [];
+              for (var i = 1; i < forecast.length; i++) {
+                temp = forecast[i];
+                var future = {};
+                future.week = temp.date.slice(3);
+                future.type = typeIcon[temp.type];
+                var wendurange = temp.low.split(' ')[1] + "-" + temp.high.split(' ')[1];
+                future.wendu = wendurange;
+                future.typeTetx = temp.type;
+                futureList.push(future);
               }
-            },
-            fail: function (err) {
-              console.error(err);
+
+              weather.futureList = futureList;
+              console.log(weather);
+              that.setData({
+                weather: weather,
+                Loadinghidden: true
+              });
             }
+          }).catch(res => {
+            console.log(res);
           });
         });
       },
